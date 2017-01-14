9c1cdf1f613ee45a42d1a19a8d7aaa7beeec2627
hpoggie <henry.poggie@gmail.com>
2017-01-13 21:49:02 -0500

    Make PlayerTest.py use unittest

--- tests/PlayerTest.py
+++ tests/PlayerTest.py
@@ -1,13 +1,19 @@
-class DuplicateCardError (Exception):
-    def __init__(self, card):
-        self.card = card
-
-    def __print__(self):
-        print "Card " + card + " appears more than once."
+import unittest
+import sys
+sys.path.insert(0, "..")
 
 
-def checkDeckForDuplicates(deck):
-    for i, card in enumerate(deck):
-        for j, card2 in enumerate(deck, start=i+1):
-            if card == card2:
-                raise DuplicateCardError(card)
+class PlayerTest(unittest.TestCase):
+    def checkDeckForDuplicates(self, deck):
+        for i, card in enumerate(deck):
+            for card2 in deck[i+1:]:
+                if card == card2:
+                    self.fail('' + card.name + ' equals ' + card2.name)
+
+    def testForDuplicates(self):
+        from core.player import Player
+        player = Player("Test Player")
+        self.checkDeckForDuplicates(player.deck)
+
+if __name__ == '__main__':
+    unittest.main()
