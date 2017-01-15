12bc60951b50ab3c34f33d5f938be450d4acf1cb
hpoggie <henry.poggie@gmail.com>
2017-01-14 20:16:31 -0500

    Add test for duplicates between players

--- tests/core.py
+++ tests/core.py
@@ -15,5 +15,15 @@ class PlayerTest(unittest.TestCase):
         player = Player("Test Player")
         self.checkDeckForDuplicates(player.deck)
 
+    def testForDuplicatesBetweenPlayers(self):
+        from core.player import Player
+        player1 = Player("Player 1")
+        player2 = Player("Player 2")
+
+        for card1 in player1.deck:
+            for card2 in player2.deck:
+                if card1 == card2:
+                    self.fail('' + card1.name + ' equals ' + card2.name)
+
 if __name__ == '__main__':
     unittest.main()
