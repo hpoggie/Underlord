652db79f042425cdf7a089e9121422ef9c1fda6f
hpoggie <henry.poggie@gmail.com>
2017-01-29 17:13:10 -0500

    Clean tests

--- tests/core.py
+++ tests/core.py
@@ -4,18 +4,21 @@ from core.player import Player
 from core.core import Game
 from factions import base
 from factions.templars import Templars
+from core.enums import *
+
+
+def deckContainsDuplicates(deck):
+    for i, card in enumerate(deck):
+        for card2 in deck[i+1:]:
+            if card == card2:
+                return True
+    return False
 
 
 class PlayerTest(unittest.TestCase):
-    def checkDeckForDuplicates(self, deck):
-        for i, card in enumerate(deck):
-            for card2 in deck[i+1:]:
-                if card == card2:
-                    self.fail('' + card.name + ' equals ' + card2.name)
-
     def testForDuplicates(self):
         player = Player(Templars)
-        self.checkDeckForDuplicates(player.deck)
+        self.failIf(deckContainsDuplicates(player.deck))
 
     def testForDuplicatesBetweenPlayers(self):
         player1 = Player(Templars)
@@ -40,6 +43,7 @@ class ActionsTest(unittest.TestCase):
         game.endPhase(player)
         game.endPhase(player)
         player.play(newCard)
+        self.failUnlessEqual(newCard.zone, Zone.facedown)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
