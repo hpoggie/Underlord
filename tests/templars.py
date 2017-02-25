78e316f4b2f46b8c1d244f34dbc7560507def2ce
hpoggie <henry.poggie@gmail.com>
2017-02-25 18:49:03 -0500

    Test templars

--- tests/templars.py
+++ tests/templars.py
@@ -1,7 +1,9 @@
 import context
 import unittest
 import factions.templars as templars
+import factions.base as base
 import core.core
+from core.enums import *
 
 
 class TestImport(unittest.TestCase):
@@ -21,11 +23,44 @@ class TestGrail(unittest.TestCase):
         self.player1 = self.game.players[0]
         self.player2 = self.game.players[1]
 
-        grail = templars.grail()
-        grail.owner = self.player
-        self.player.deck = [grail]
+        self.grail = templars.grail()
+        self.grail.owner = self.player1
+        self.player1.deck = [self.grail]
+
+        self.one = base.one()
+        self.one.owner = self.player2
+        self.player2.deck = [self.one]
 
         self.game.start()
+
+    def testGrail(self):
+        self.player1.endPhase()
+        self.player1.endPhase()
+        self.player1.endPhase()
+        self.player1.play(self.grail)
+        self.player1.endPhase()
+
+        self.player2.endPhase()
+        self.player2.endPhase()
+        self.player2.endPhase()
+        self.player2.play(self.one)
+        self.player2.endPhase()
+
+        self.player1.revealFacedown(self.grail)
+        self.player1.endPhase()
+        self.player1.endPhase()
+        self.player1.endPhase()
+        self.player1.endPhase()
+
+        self.player2.revealFacedown(self.one)
+        self.player2.endPhase()
+        self.player2.endPhase()
+        self.player2.endPhase()
+        try:
+            self.player2.attack(self.one, Zone.face)
+            self.fail()
+        except IllegalMoveError:
+            pass
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
