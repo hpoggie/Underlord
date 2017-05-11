5e35bb58eaa2c4aee9fa594b05b0112370827031
hpoggie <henry.poggie@gmail.com>
2017-05-11 15:39:30 -0400

    Add a lot of cards

--- factions/templars.py
+++ factions/templars.py
@@ -1,5 +1,5 @@
 import base
-from core.card import Card, Faction
+from core.card import Card, Faction, TargetedAbility
 from core.enums import Zone
 from core.core import IllegalMoveError
 
@@ -29,6 +29,69 @@ def equus():
 
     return equus
 
+def archangel():
+    return Card(
+            name="Archangel",
+            image="angel-wings.png",
+            cost=13,
+            rank=15
+            )
+
+def holyHandGrenade():
+    hhg = Card(
+            name="Holy Hand Grenade",
+            image="holy-hand-grenade.png",
+            playsFaceUp=True,
+            cost=5,
+            spell=True
+            )
+
+    def _onSpawn(self, target):
+        self.game.destroy(target)
+        self.moveZone(target)
+
+    hhg.onSpawn=TargetedAbility(
+        func=_onSpawn,
+        card=hhg
+        )
+
+    return hhg
+
+def wrathOfGod():
+    return Card(
+            name="Wrath of God",
+            image="wind-hole.png",
+            cost=5,
+            spell=True,
+            playsFaceUp=True,
+            onSpawn=base.sweepAbility
+            )
+
+def corvus():
+    def _onSpawn(self):
+        self.owner.manaCap += 1
+
+    return Card(
+            name="Corvus",
+            image="raven.png",
+            cost=1,
+            rank=1,
+            onSpawn=_onSpawn
+            )
+
+def miracle():
+    def _onSpawn(self):
+        while(len(self.owner.hand) < 5):
+            self.owner.drawCard()
+
+    return Card(
+            name="Miracle",
+            image="sundial.png",
+            cost=6,
+            spell=True,
+            onSpawn=_onSpawn
+            )
+
 
 Templars = Faction(
     name="Templars",
@@ -39,7 +102,10 @@ Templars = Faction(
         base.sweep(), base.sweep(),
         base.spellBlade(),
         strix(),
-        equus(),
-        equus(),
+        equus(), equus(),
+        corvus(),
+        holyHandGrenade(),
+        wrathOfGod(),
+        archangel()
         ],
     )
