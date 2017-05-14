3c76475d46adceebb79bcd6400e826d48191fe1b
hpoggie <henry.poggie@gmail.com>
2017-05-14 11:54:01 -0400

    Remove Strix

--- factions/templars.py
+++ factions/templars.py
@@ -4,15 +4,6 @@ from core.enums import Zone
 from core.core import IllegalMoveError
 
 
-def strix():
-    return Card(
-        name="Strix",
-        image="owl.png",
-        cost=1,
-        rank=1
-        )
-
-
 def equus():
     import types
 
@@ -97,7 +88,6 @@ Templars = Faction(
         base.one(), base.one(), base.one(), base.one(),
         base.sweep(), base.sweep(),
         base.spellBlade(),
-        strix(),
         equus(), equus(),
         corvus(),
         holyHandGrenade(),
