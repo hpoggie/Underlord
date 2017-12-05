e6c689fbfc35a261b6e0c5a78b56b91fff0c5803
hpoggie <henry.poggie@gmail.com>
2017-12-04 22:13:07 -0500

    Put MouseHandler in a separate file

---
+++ mouse.py
@@ -0,0 +1,76 @@
+from direct.showbase.DirectObject import DirectObject
+from panda3d.core import CollisionNode, GeomNode, CollisionRay
+
+
+class MouseHandler (DirectObject):
+    def __init__(self):
+        self.accept('mouse1', self.onMouse1, [])
+
+        self.showCollisions = False
+
+        pickerNode = CollisionNode('mouseRay')
+        pickerNP = camera.attachNewNode(pickerNode)
+        pickerNode.setFromCollideMask(GeomNode.getDefaultCollideMask())
+        self.pickerRay = CollisionRay()
+        pickerNode.addSolid(self.pickerRay)
+        base.cTrav.addCollider(pickerNP, base.handler)
+        if self.showCollisions:
+            base.cTrav.showCollisions(render)
+        base.disableMouse()
+
+        self.activeCard = None
+        self.targeting = False
+
+    def getObjectClickedOn(self):
+        if base.mouseWatcherNode.hasMouse():
+            mpos = base.mouseWatcherNode.getMouse()
+            self.pickerRay.setFromLens(base.camNode, mpos.getX(), mpos.getY())
+
+            base.cTrav.traverse(render)
+            if (base.handler.getNumEntries() > 0):
+                base.handler.sortEntries()
+                pickedObj = base.handler.getEntry(0).getIntoNodePath()
+                pickedObj = pickedObj.findNetTag('zone')
+                return pickedObj
+
+    def doClick(self):
+        pickedObj = self.getObjectClickedOn()
+
+        if self.targeting:
+            if pickedObj is not None:
+                base.acceptTarget(pickedObj)
+                self.targeting = False
+            return
+
+        if pickedObj and not pickedObj.isEmpty():
+            if pickedObj.getTag('zone') == 'hand':
+                try:
+                    base.playCard(pickedObj)
+                except IllegalMoveError as error:
+                    print(error)
+            elif pickedObj.getTag('zone') == 'face-down':
+                if not self.activeCard:
+                    try:
+                        base.revealFacedown(pickedObj)
+                    except IllegalMoveError as error:
+                        print(error)
+                else:
+                    self.activeCard = None
+            elif pickedObj.getTag('zone') == 'enemy face-down':
+                if self.activeCard:
+                    base.attack(self.activeCard, pickedObj)
+                    self.activeCard = None
+            elif pickedObj.getTag('zone') == 'face-up':
+                if base.phase == Phase.play and not self.activeCard:
+                    self.activeCard = pickedObj
+                elif self.activeCard:
+                    base.attack(self.activeCard, pickedObj)
+                    self.activeCard = None
+            elif pickedObj.getTag('zone') == 'face' and self.activeCard:
+                    base.attack(self.activeCard, pickedObj)
+                    self.activeCard = None
+        else:
+            self.activeCard = None
+
+    def onMouse1(self):
+        self.doClick()
