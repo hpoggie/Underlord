a4d9b87d3643512e0fc4aba426f283837eff15a3
hpoggie <henry.poggie@gmail.com>
2018-01-25 19:21:50 -0500

    No need for range(0, whatever)

--- core/enums.py
+++ core/enums.py
@@ -1,5 +1,5 @@
 def numericEnum(*items):
-    d_items = dict(list(zip(items, list(range(0, len(items))))))
+    d_items = dict(list(zip(items, list(range(len(items))))))
     d_items['keys'] = items
     return type('NumericEnum', (), d_items)
 
