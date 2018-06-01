c288bd3aae361ba41b232960e8da003c30cdcf7c
hpoggie <henry.poggie@gmail.com>
2018-06-01 10:40:28 -0400

    newGame now lets you use factions

--- tests/util.py
+++ tests/util.py
@@ -17,12 +17,20 @@ def newGame(*args):
     """
     Helper function for writing cleaner tests.
     Tries to intelligently create a new game from the arguments you give it.
+    If you give it 2 factions, it will make a game with those factions
+    If you give it one faction, both players will be that faction
     If you give it 2 lists, it will give the players those lists as decks.
     If you give it one list, it will make that the deck for both players.
     If you give it some cards, it will make those the deck for both players.
     """
+    if (len(args) == 2 and
+            isinstance(args[0], type) and
+            isinstance(args[1], type)):
+        game = Game(*args)
+    elif len(args) == 1 and isinstance(args[0], type):
+        game = Game(args[0], args[0])
     # If we got 2 arguments and at least one of them is a list
-    if len(args) == 2 and len(
+    elif len(args) == 2 and len(
             [x for x in args if isinstance(x, collections.Iterable)]) > 0:
         # For each of the arguments, if it's a list, make that the player's
         # deck, otherwise make a list containing only that card and make it
