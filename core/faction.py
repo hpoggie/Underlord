8b0f3c7515570cdbdd019c0c5cb1d5ecaa4ff7f1
hpoggie <henry.poggie@gmail.com>
2018-06-03 09:48:07 -0400

    Fix decks being constructed wrong

--- core/faction.py
+++ core/faction.py
@@ -6,7 +6,7 @@ def deck(*args):
 
     for i, arg in enumerate(args):
         if hasattr(arg, '__call__'):
-            if i + 1 < len(args) and args[i + 1] is int:
+            if i + 1 < len(args) and isinstance(args[i + 1], int):
                 deck += [arg() for i in range(args[i + 1])]
             else:
                 deck.append(arg())
