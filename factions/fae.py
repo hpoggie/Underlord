8b419ce33e1cf9ddbba314ac29a41d8fda90c99c
hpoggie <henry.poggie@gmail.com>
2018-07-21 17:26:08 -0400

    Add Oberon's Guard

--- factions/fae.py
+++ factions/fae.py
@@ -1,4 +1,5 @@
 from core.player import Player
+from core.exceptions import InvalidTargetError
 from core.faction import deck
 from core.card import Card
 import factions.base as base
@@ -13,8 +14,23 @@ class faerieMoth(Card):
     desc = "Fast."
 
 
+class oberonsGuard(Card):
+    name = "Oberon's Guard"
+    icon = 'elf-helmet.png'
+    cost = 2
+    rank = 2
+    desc = ("When this spawns, you may return target face-down card you "
+            "control to its owner's hand.")
+
+    def onSpawn(self, target):
+        if target.zone is not self.controller.facedowns:
+            raise InvalidTargetError()
+
+        target.zone = target.owner.hand
+
+
 class Faerie(Player):
-    deck = deck(faerieMoth, 5) + base.deck
+    deck = deck(faerieMoth, 5, oberonsGuard, 4) + base.deck
 
     def endPhase(self, card=None):
         self.failIfInactive()
