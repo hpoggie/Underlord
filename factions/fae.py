9bb207284b05bafe130ffc0ea35c1aa340093422
hpoggie <henry.poggie@gmail.com>
2018-07-21 15:17:47 -0400

    Add Faerie Moth

--- factions/fae.py
+++ factions/fae.py
@@ -1,9 +1,20 @@
 from core.player import Player
+from core.faction import deck
+from core.card import Card
 import factions.base as base
 
 
+class faerieMoth(Card):
+    name = "Faerie Moth"
+    icon = 'butterfly.png'
+    cost = 1
+    rank = 1
+    fast = True
+    desc = "Fast."
+
+
 class Faerie(Player):
-    deck = base.deck
+    deck = deck(faerieMoth, 5) + base.deck
 
     def endPhase(self, card=None):
         self.failIfInactive()
